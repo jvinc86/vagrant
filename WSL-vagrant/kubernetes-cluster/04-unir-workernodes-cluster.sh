a3febdcc304dffe9e8f15ca5cec732c7fcd4d9e2
Vincent <jvincent.camacho@gmail.com>
2022-06-01 08:17:26 +0200

    NewChanges

--- WSL-vagrant/kubernetes-cluster/04-unir-workernodes-cluster.sh
+++ WSL-vagrant/kubernetes-cluster/04-unir-workernodes-cluster.sh
@@ -4,8 +4,10 @@
 echo "[PASO 1]: Instalar paquete SSHPass"
 yum install sshpass -y
 
+
 echo "[PASO 2]: Extraer desde Master el comando para unir (join) el Worker Node al Cluster"
 sshpass -p 'vagrant' scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no vagrant@kmaster:incluir_nodo_a_cluster.sh /root/incluir_nodo_a_cluster.sh
+
 
 echo "[PASO 3]: Correr el comando para unir (join) el Worker Node al Cluster"
 bash /root/incluir_nodo_a_cluster.sh
