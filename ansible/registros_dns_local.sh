7f262959d9441ae4d5ea6b8ea1493355e3c02f9e
jvinc86 <jvincent.camacho@gmail.com>
2022-06-03 09:43:29 +0200

    NewChanges

---
+++ ansible/registros_dns_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat << EOF | sudo tee -a /etc/hosts
+
+# Ansible Infra
+192.168.56.70 ansiblemaster  ansiblemaster.vincenup.com  ansible
+192.168.56.71 srvubuntu1     srvubuntu1.vincenup.com
+192.168.56.72 srvubuntu2     srvubuntu2.vincenup.com
+192.168.56.61 srvrocky1      srvrocky1.vincenup.com
+192.168.56.62 srvrocky2      srvrocky2.vincenup.com
+EOF
